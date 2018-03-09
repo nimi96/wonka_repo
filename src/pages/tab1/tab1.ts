0d2141a76b515d0e0c977c82cd429e40f70f610a
nimi96 <pamnaninimit@ymail.com>
2018-03-09 20:25:08 +0530

    try1 repo

--- src/pages/tab1/tab1.ts
+++ src/pages/tab1/tab1.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import * as PouchDB from 'pouchdb';
-
+import * as moment from 'moment';
 
 
 @IonicPage()
